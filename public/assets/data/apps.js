607e2a52d80dbe16c1b52cf489264ec3983a4f2f
FortNerdYT <141072739+FortNerdYT@users.noreply.github.com>
2025-04-01 16:44:20 -0400

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -5,13 +5,6 @@ var apps = [
       'image': 'assets/images/add.png'
     },
     {
-      "id": "google",
-      "title": "Google",
-      "url": "https://www.google.com/?safe=active&ssui=on",
-      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
-      
-    },
-    {
       "id": "discord",
       "title": "Discord",
       "url": "https://discord.com/app",
@@ -88,34 +81,6 @@ var apps = [
       'title': 'Cool Math Games',
       'url': 'https://coolmathgames.com',
       'image': 'https://docs.google.com/drawings/d/e/2PACX-1vR6y2GvcGu9PdrX9zzE24dhm24hClcNsdAmZjIgSUC8sFOahf6t7Yg6l_W8Rd4GduWOH_X02GHVFdBb/pub?w=512&h=512'
-    },
-    {
-      'id': 'win11',
-      'title': 'Windows 11 in React',
-      'url': 'https://win11.blueedge.me/',
-      "image": "https://cdn.upload.systems/uploads/lnysPmIS.png",
-      "description": "A remake of the Windows 11 UI in React.js"
-    },
-    {
-      'id': 'win10',
-      'title': 'Windows 10 in React',
-      'url': 'https://win-10.vercel.app/',
-      "image": "https://downlody.com/windows/files/image/Windows-10-icon.png",
-      "description": "Windows 10 recreation in React.js"
-    },
-    {
-      'id': 'winxp',
-      'title': 'Windows XP in React',
-      'url': 'https://www.oonjaa.com/',
-      "image": "https://archive.org/services/img/1d9464dbff7a1630882d0e2090cb714c",
-      "description": "Windows XP recreation in React.js"
-    },
-    {
-      'id': 'gbaemulator',
-      'title': 'GBA Emulator',
-      'url': 'https://cattn.github.io/gba',
-      "image": "https://cattn.github.io/gba/pwa/apple-icon-180.png",
-      "description": "A Game Boy Advance retro emulator with over 3000 games, and other consoles as well"
     },
     {
       'id': 'xboxcloudgaming',
